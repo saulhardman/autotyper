74f49a496722460f47953662bffd279b7f1ea454
Saul Hardman <hello@iamsaul.co.uk>
2017-01-07 23:08:36 +0100

    fix: typo in `upperCaseFirstLetter`
    - change `toLowerCase` to `toUpperCase`

--- src/upper-case-first-letter.js
+++ src/upper-case-first-letter.js
@@ -1,4 +1,4 @@
 export default function upperCaseFirstLetter(string) {
   // e.g. text => Text
-  return `${string.substring(0, 1).toLowerCase()}${string.substring(1)}`;
+  return `${string.substring(0, 1).toUpperCase()}${string.substring(1)}`;
 }
