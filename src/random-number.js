d9905f50ef2b709bcdeb9a0e51ea4ed1eaeffed0
Saul Hardman <hello@iamsaul.co.uk>
2017-01-08 10:38:59 +0100

    fix: ensure `randomNumber` is range inclusive

--- src/random-number.js
+++ src/random-number.js
@@ -1,4 +1,8 @@
 export default function randomNumber(min, max) {
-  // return a random number between min and max
-  return Math.floor(Math.random() * (max - (min + 1))) + min;
+  // return a random number between min and max (inclusive of min and max)
+  const minimum = Math.min(min, max);
+  const maximum = Math.max(min, max);
+  const multiplier = (maximum - minimum) + 1;
+
+  return Math.floor((Math.random() * multiplier)) + minimum;
 }
