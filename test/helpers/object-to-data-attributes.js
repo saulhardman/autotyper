589cde3e49829a266632376c0d461dd26a3d6859
Saul Hardman <hello@iamsaul.co.uk>
2017-01-08 22:24:51 +0100

    chore: add `objectToDataAttributes` test helper

---
+++ test/helpers/object-to-data-attributes.js
@@ -0,0 +1,7 @@
+export default function objectToDataAttributes(object, namespace) {
+  return Object.keys(object).map((name) => {
+    const value = object[name];
+
+    return `data-${namespace}-${name}='${JSON.stringify(value)}'`;
+  }).join(' ');
+}
