c78aa5a73105b8d5a7977929655fb4439f523caf
Saul Hardman <hello@iamsaul.co.uk>
2017-01-08 10:40:46 +0100

    test: add tests for `interval` module

---
+++ src/interval.test.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+import interval from '../src/interval';
+
+test('it works with a number', (t) => {
+  const value = 100;
+  const result = interval(value);
+
+  t.is(result, 100);
+});
+
+test('it works with an array', (t) => {
+  const value = [100, 200];
+  const result = interval(value);
+
+  if (result >= 100 && result <= 200) {
+    t.pass();
+  } else {
+    t.fail();
+  }
+});
+
+test('it works with a function', (t) => {
+  const value = () => 100;
+  const result = interval(value);
+
+  t.is(result, 100);
+});
