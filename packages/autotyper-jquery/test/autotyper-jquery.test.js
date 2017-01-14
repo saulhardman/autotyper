06d7371e3019974d6564ce9aa6dea1e2f78388ad
Saul Hardman <hello@iamsaul.co.uk>
2017-01-14 17:54:39 +0100

    test: remove comments and unused vars

--- packages/autotyper-jquery/test/autotyper-jquery.test.js
+++ packages/autotyper-jquery/test/autotyper-jquery.test.js
@@ -1,11 +1,7 @@
 import test from 'ava';
 import jQuery from 'jquery';
 
-import { EVENTS, NAME } from 'autotyper';
 import '../src/index';
-
-const EVENT_NAMES = Object.keys(EVENTS).map(event => EVENTS[event]);
-const { LOOP } = EVENTS;
 
 test.beforeEach((t) => {
   const text = 'Example text.';
@@ -28,29 +24,3 @@ test('does autotyper extend jQuery', (t) => {
 
   t.is(typeof jQuery(element).autotyper, 'function');
 });
-
-// test('is the autotyper instance on the jQuery element');
-
-// test('are autotyper events proxied to jQuery events', (t) => {
-//   const { element } = t.context;
-//   const $element = jQuery(element);
-//   const options = { loop: 1 };
-//
-//   t.plan(EVENT_NAMES.length);
-//
-//   EVENT_NAMES.forEach((event) => {
-//     const namespacedEvent = `${NAME}:${event}`;
-//
-//     $element.on(namespacedEvent, () => {
-//       $element.off(namespacedEvent);
-//
-//       t.pass();
-//     });
-//   });
-//
-//   $element.autotyper(options);
-//
-//   const autotyper = $element.data(NAME);
-//
-//   autotyper.on(LOOP, () => autotyper.destroy());
-// });
