24e871d8d1e758ea58cf448f0bdef6849b7a049e
Saul Hardman <hello@iamsaul.co.uk>
2017-01-19 08:14:01 +0100

    test(autotyper-jquery): add tests

--- packages/autotyper-jquery/test/autotyper-jquery.test.js
+++ packages/autotyper-jquery/test/autotyper-jquery.test.js
@@ -1,7 +1,17 @@
-import test from 'ava';
+import autotyper, { DEFAULTS, EVENTS, NAME } from 'autotyper';
 import jQuery from 'jquery';
+import test from 'ava';
 
 import '../src/index';
+
+const EVENT_NAMES = Object.keys(EVENTS).map(event => EVENTS[event]);
+const { DESTROY, STOP } = EVENTS;
+const ASYNC_TIMEOUT = 5000;
+
+// replicate default `loopInterval` assignment from `init()`
+Object.assign(DEFAULTS, {
+  loopInterval: DEFAULTS.interval,
+});
 
 test.beforeEach((t) => {
   const text = 'Example text.';
@@ -10,12 +20,18 @@ test.beforeEach((t) => {
     <p class="js-example">${text}</p>
   `;
 
-  const element = document.querySelector('.js-example');
+  const element = jQuery(document.querySelector('.js-example'));
 
   Object.assign(t.context, { element });
 });
 
-test('does autotyper extend jQuery', (t) => {
+test.afterEach((t) => {
+  const { element } = t.context;
+
+  element.autotyper('destroy');
+});
+
+test('autotyper extends jQuery', (t) => {
   const { element } = t.context;
 
   t.plan(2);
@@ -23,4 +39,51 @@ test('does autotyper extend jQuery', (t) => {
   t.is(typeof jQuery.fn.autotyper, 'function');
 
   t.is(typeof jQuery(element).autotyper, 'function');
+});
+
+test('the instance is accessible via jQuery element data', (t) => {
+  const { element } = t.context;
+
+  element.autotyper();
+
+  const instance = element.data(NAME);
+
+  t.true(Object.isPrototypeOf.call(autotyper, instance));
+});
+
+test('it proxies events to jQuery', (t) => {
+  const { element } = t.context;
+  const options = { loop: 1, interval: 50 };
+
+  t.plan(EVENT_NAMES.length);
+
+  EVENT_NAMES.forEach((event) => {
+    element.one(`${NAME}:${event}`, () => t.pass());
+  });
+
+  element.autotyper(options);
+
+  const instance = element.data(NAME);
+
+  instance.on(STOP, () => instance.destroy());
+
+  return new Promise((resolve, reject) => {
+    instance.on(DESTROY, resolve);
+
+    setTimeout(reject, ASYNC_TIMEOUT);
+  });
+});
+
+test('it can call functions via the jQuery plugin', (t) => {
+  const { element } = t.context;
+
+  element.autotyper();
+
+  element.autotyper('stop');
+
+  t.false(element.data('autotyper').isRunning);
+});
+
+test('it works without an element', (t) => {
+  t.deepEqual(jQuery.autotyper().settings, DEFAULTS);
 });
