762dabf2e1a1588e201d22c7260186efe8d51fb1
Saul Hardman <hello@iamsaul.co.uk>
2017-01-20 08:02:29 +0100

    chore: add `eslint-plugin-html`
    - lint development `index.html`
    - remove `module.exports` override in development

--- packages/autotyper/src/index.js
+++ packages/autotyper/src/index.js
@@ -6,7 +6,3 @@ export {
   NAME,
   VERSION,
 } from './autotyper';
-
-if (process.env.NODE_ENV === 'development') {
-  module.exports = require('./autotyper').default; // eslint-disable-line global-require
-}
