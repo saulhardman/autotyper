5160b07772031e1d6e176219afc8963e8865ae92
Saul Hardman <hello@iamsaul.co.uk>
2020-01-20 23:00:13 +0100

    chore: update release flow

--- scripts/commit.js
+++ scripts/commit.js
@@ -2,7 +2,4 @@ const git = require('simple-git')(process.cwd());
 
 const { version } = require('../lerna.json');
 
-git
-  .add('./*')
-  .commit(version)
-  .addAnnotatedTag(`v${version}`, version);
+git.add('./*').commit(version, { '--amend': null });
